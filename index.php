6c890f71ac4b3d75f744de50bcad37cdbba25ff4
Carolina <carolzuniga@gmail.com>
2025-11-21 16:25:00 -0600

    Pruebajenkins2

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include("db.php");
 </head>
 <body>
 <div class="container">
-    <h2>Lista de Clientes ::</h2>
+    <h2>Lista de Clientes hola ::</h2>
     <table>
         <tr>
             <th>ID</th>
